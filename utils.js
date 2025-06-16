daa3847dfd32c5a251a5bbee07dfe0db774b5ab6
chunmeifeng <75488183+chunmeifeng@users.noreply.github.com>
2025-06-17 04:26:38 +0800

    v1

--- utils.js
+++ utils.js
@@ -1,27 +1,32 @@
 function createProjectElement(id, project){
-    /* Create an element in Project
-    tag: some tags
-    title: title of the project or publications
-    paper_url: link to the paper
-    authors: authors
-    conference: publication venue
-    image (optional): directory to project image
-    others (optional): anything else
-    */
     if (project.tag == null)
         project.tag = id;
     if (project.others == null)
         project.others = "";
 
-    html_img = `<img src='${project.image}' style="max-width: 160px"></div></div>`
-    html_txt = `<p>
-      <a href="${project.paper_url}"><papertitle>${project.title}</papertitle></a>
-      <br>
-      ${project.authors}
-      <br>
-      <em>${project.conference}</em>
-      <br>
-      ${project.others}`
+    /* ---------- name bold + symbolism upper ---------- */
+    // bold name
+    project.authors = project.authors.replace(
+        /Chun[–-]Mei\s+Feng/gi,        
+        '<strong>Chun-Mei&nbsp;Feng</strong>'
+    );
+
+    // symbolism upper
+    project.authors = project.authors
+        .replace(/\*/g,'<sup>*</sup>')
+        .replace(/†/g,'<sup>†</sup>');
+    
+    project.conference = project.conference
+    .replace(/Spotlight/gi,    '<span class="flag-red">Spotlight</span>')
+    .replace(/Early\s+Accept/gi, '<span class="flag-red">Early&nbsp;Accept</span>')
+    .replace(/Oral/gi,         '<span class="flag-red">Oral</span>');
+
+    const html_img = `<img src='${project.image}' style="max-width: 160px">`;
+    const html_txt = `<p>
+      <a href="${project.paper_url}"><papertitle>${project.title}</papertitle></a><br>
+      ${project.authors}<br>
+      <em>${project.conference}</em><br>
+      ${project.others}`;
 
     document.getElementById(id + "-img").innerHTML = html_img;
     document.getElementById(id + "-txt").innerHTML = html_txt;
